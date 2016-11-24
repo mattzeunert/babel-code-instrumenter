4d3f89355490fd4a181d6c7a295e8ec686b0ce6c
Matt Zeunert <matt@mostlystatic.com>
2016-11-24 23:35:02 +0000

    Start injecting script.

--- background.js
+++ background.js
@@ -3,12 +3,26 @@ var codeInstrumenter = new ChromeCodeInstrumenter({
 });
 
 function onBrowserActionClicked(tab) {
-	chrome.storage.local.get(function(data){
+	chrome.storage.local.get(null, function(data){
 		var pluginCode = data.plugins[data.selectedPluginIndex].babelPlugin
 		var babelPlugin = eval("(" + pluginCode + ")")
 
 	    codeInstrumenter.toggleTabInstrumentation(tab.id, {
-			babelPlugin
+			babelPlugin,
+			onBeforePageLoad: function(callback){
+				debugger
+		        this._executeScript(`
+		            var script2 = document.createElement("script")
+		            script2.text = 'window.sth = function(){ console.log("str lit"); }'
+		            document.documentElement.appendChild(script2)`
+		        , function(){
+		            // ideally we'd wait for a message from the page,
+		            // but for now this will work
+		            setTimeout(function(){
+		                callback();
+		            },100)
+		        })
+		    }
 		})
 	})
 }
